2ee4b63d324295ee46fc38701475a20b7a6a8764
Henry Millison <hmillison@gmail.com>
2021-05-11 15:00:54 -0400

    add a toggle for the old geocities theme

---
+++ scripts/easter-eggs.js
@@ -0,0 +1,24 @@
+// Available stylesheets for the website
+const THEMES = {
+  main: './styles/main.css',
+  geocities: './styles/geocities.css',
+};
+
+function getStylesheet() {
+  return document.querySelector('#stylesheet');
+}
+
+/**
+ * Use this function to switch the theme back from fun to serious
+ * For the sake of simplicity, exposing this in the global scope for now
+ */
+function toggleTheme() {
+  const theme = getStylesheet();
+  const isDefaultThemeVisible = theme.getAttribute('href').includes(THEMES.main);
+
+  if (isDefaultThemeVisible) {
+    theme.setAttribute('href', THEMES.geocities);
+  } else {
+    theme.setAttribute('href', THEMES.main);
+  }
+}
